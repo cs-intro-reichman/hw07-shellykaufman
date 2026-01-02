cd66697c799e0fa981b1a8711f0e524eb68ab900
Shelly Kaufman <shelly.kaufman01@post.runi.ac.il>
2026-01-02 16:42:38 +0200

    complited assignment

--- Koch.java
+++ Koch.java
@@ -22,13 +22,36 @@ public class Koch {
 		// Gets n, and draws a Koch snowflake of n edges in the standard canvass.
 		snowFlake(Integer.parseInt(args[0]));
 		*/
+
+		if (args.length == 1) {
+            snowFlake(Integer.parseInt(args[0]));
+		}
+		else if (args.length == 5) {
+            curve(Integer.parseInt(args[0]),
+                  Double.parseDouble(args[1]), Double.parseDouble(args[2]), 
+                  Double.parseDouble(args[3]), Double.parseDouble(args[4]));
+        }  
 	}
 
 	/** Gets n, x1, y1, x2, y2,
      *  and draws a Koch curve of depth n from (x1,y1) to (x2,y2). */
 	public static void curve(int n, double x1, double y1, double x2, double y2) {
 		StdDraw.line(x1, y1, x2, y2);
-		//// Write the rest of your code below.
+		if (n == 0) {
+				StdDraw.line(x1, y1, x2, y2);
+				return;
+			}
+    double xOneThird = (2 * x1 + x2) / 3.0;
+    double yOneThird = (2 * y1 + y2) / 3.0;
+    double xTwoThirds = (x1 + 2 * x2) / 3.0;
+    double yTwoThirds = (y1 + 2 * y2) / 3.0;
+    double xCodcod = (Math.sqrt(3) / 6.0) * (y1 - y2) + 0.5 * (x1 + x2);
+    double yCodcod = (Math.sqrt(3) / 6.0) * (x2 - x1) + 0.5 * (y1 + y2);
+    curve(n - 1, x1, y1, xOneThird, yOneThird);
+    curve(n - 1, xOneThird, yOneThird, xCodcod, yCodcod);
+    curve(n - 1, xCodcod, yCodcod, xTwoThirds, yTwoThirds);
+    curve(n - 1, xTwoThirds, yTwoThirds, x2, y2);	
+	
 	}
 
     /** Gets n, and draws a Koch snowflake of n edges in the standard canvass. */
@@ -36,7 +59,16 @@ public class Koch {
 		// A little tweak that makes the drawing look better
 		StdDraw.setYscale(0, 1.1);
 		StdDraw.setXscale(0, 1.1);
-		// Draws a Koch snowflake of depth n
-		//// Write the rest of your code below.
+		// Draws a Koch snowflake of depth n    
+        double x1 = 0.2;
+		double y1 = 0.25;
+        double x2 = 0.8;
+		double y2 = 0.25;
+        double x3 = 0.5;
+		double y3 = 0.25 + (Math.sqrt(3) / 2.0) * 0.6;
+
+        curve(n, x1, y1, x3, y3);
+        curve(n, x3, y3, x2, y2);
+        curve(n, x2, y2, x1, y1);
 	}
 }
--- Sierpinski.java
+++ Sierpinski.java
@@ -7,12 +7,44 @@ public class Sierpinski {
 	
 	// Draws a Sierpinski triangle of depth n on the standard canvass.
 	public static void sierpinski (int n) {
-		//// Replace this comment with your code
+
+		double x1 = 0.0;
+        double y1 = 0.0;
+        double x2 = 1.0;
+        double y2 = 0.0;
+        double x3 = 0.5;
+        double y3 = Math.sqrt(3) / 2.0; 
+
+        StdDraw.setPenColor(StdDraw.BLACK);
+        double[] xCoords = { x1, x2, x3 };
+        double[] yCoords = { y1, y2, y3 };
+        StdDraw.polygon(xCoords, yCoords);
+
+        sierpinski(n, x1, x2, x3, y1, y2, y3);	
 	}
 	
 	// Does the actual drawing, recursively.
 	private static void sierpinski(int n, double x1, double x2, double x3,
 		                                 double y1, double y2, double y3) {
-		//// Replace this comment with your code
+		if (n <= 0) {
+            return;
+        }
+		double midBottomX = (x1 + x2) / 2.0;
+        double midBottomY = (y1 + y2) / 2.0;
+        
+        double midRightX = (x2 + x3) / 2.0;
+        double midRightY = (y2 + y3) / 2.0;
+        
+        double midLeftX = (x3 + x1) / 2.0;
+        double midLeftY = (y3 + y1) / 2.0;
+
+        StdDraw.setPenColor(StdDraw.WHITE);
+        double[] xMidPoints = { midBottomX, midRightX, midLeftX };
+        double[] yMidPoints = { midBottomY, midRightY, midLeftY };
+        StdDraw.filledPolygon(xMidPoints, yMidPoints);
+
+        sierpinski(n - 1, x1, midBottomX, midLeftX, y1, midBottomY, midLeftY);
+        sierpinski(n - 1, midBottomX, x2, midRightX, midBottomY, y2, midRightY);
+        sierpinski(n - 1, midLeftX, midRightX, x3, midLeftY, midRightY, y3);
 	}
 }
