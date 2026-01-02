e0adbbe3baa9aa8d8ae51384eeea85e523405a40
Shelly Kaufman <shelly.kaufman01@post.runi.ac.il>
2026-01-02 16:48:03 +0200

    complited assignment

--- Binomial.java
+++ Binomial.java
@@ -18,10 +18,10 @@ public class Binomial {
 	 }
 	
 	// Computes the Binomial function, efficiently
-	public static int binomial(int n, int k) {
+	public static long binomial(int n, int k) {
 		//// This function creates a 2D array, say memo, 
 		// יצירת מערך memo בגודל מתאים
-    	int[][] memo = new int[n + 1][k + 1];
+    	long[][] memo = new long[n + 1][k + 1];
 		//// and then initializes all its elements to -1.
 		for (int i = 0; i <= n; i++) {
 			for (int j = 0; j <= k; j++) {
@@ -32,7 +32,7 @@ public class Binomial {
     	return binomial(n, k, memo);
 	}
 
-	private static int binomial(int n, int k, int[][] memo) {
+	private static long binomial(int n, int k, long [][] memo) {
 		if (memo[n][k] != -1) {
 			return memo[n][k];
 		}
